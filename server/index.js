2bae7ffcda9da2bb91a426764a7ce3deacd55592
GaneshSPatil <ganeshpl@thoughtworks.com>
2017-10-07 23:13:32 +0530

    add CORS

--- server/index.js
+++ server/index.js
@@ -15,6 +15,12 @@ const employees = {
   }
 };
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/api/employee', function (req, res) {
   const employeeId = url.parse(req.url, true).query.id;
   const employee = employees[employeeId];
